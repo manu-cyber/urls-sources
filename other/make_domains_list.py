14a986bd5e57871174d89b63adc2128463aa4c49
c3manu <manu@fairydust.email>
2026-07-26 17:25:23 +0200

    Start list for Nicaragua

--- other/make_domains_list.py
+++ other/make_domains_list.py
@@ -164,6 +164,7 @@ if __name__ == '__main__':
         '43200_moldova.txt',
         '43200_myanmar.txt',
         '43200_nepal.txt',
+        '43200_nicaragua.txt',
         '43200_palestine.txt',
         '43200_poland.txt',
         '43200_politics_africa-misc.txt',
